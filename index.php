46515724728350fe9a031d03449d5b1bab8e902c
Renzo Tejada <renzzo09@gmail.com>
2016-01-03 16:37:44 -0500

    first commit de funciones utiles

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP Utils
+Plugin URI: https://github.com/RenzoTejada/wp-utils
+Description: WP Utils - Funciones de apoyo - Utilitarios
+Version: 1.0
+Author: Renzo Tejada
+Author URI: http://renzotejada.com/
+License: GPLv2
+*/
+
+include 'functions.php';
+
+
+
