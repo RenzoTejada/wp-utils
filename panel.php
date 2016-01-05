ff18e58ddd33f9df684e688767612fec05910d84
Renzo Tejada <renzo.tejada@clicksandbricks.pe>
2016-01-05 17:46:10 -0500

    test de html admin panel de wp utils

--- panel.php
+++ panel.php
@@ -1,2 +1,19 @@
-<?php
-echo "hola panel";
+<div class="wrap">
+	<h1><?php echo \WP_Utils::TITLE;?></h1>
+
+	<div id="dashboard-widgets-wrap">
+		<div id="dashboard-widgets" class="metabox-holder">
+			<div id="postbox-container-1" class="postbox-container">
+				<div id="normal-sortables" class="meta-box-sortables ui-sortable">
+					<div id="dashboard_right_now" class="postbox ">
+						
+						<h2 class="hndle ui-sortable-handle">
+							<span>De un vistazo</span>
+						</h2>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
