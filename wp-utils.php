364758ebac0a2a2778d7e804e073fcceee83ce33
Renzo Tejada <renzzo09@gmail.com>
2025-01-14 16:01:53 -0500

    fix:v3.4

--- wp-utils.php
+++ wp-utils.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Utils para WooCommerce y WordPress
  * Plugin URI:        https://renzotejada.com/utils-para-woocommerce-y-wordpress/
  * Description:       Support functions - Utilities, essential functions.
- * Version:           3.3
+ * Version:           3.4
  * Author:            Renzo Tejada
  * Author URI:        https://renzotejada.com/
  * License:           GPL-2.0-or-later
